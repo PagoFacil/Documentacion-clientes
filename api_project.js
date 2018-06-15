733bcce1d978568e28180e0a1e3d602a6f329082
Omar Alvarez <omar.alvarez@pagofacil.net>
2018-06-15 17:04:18 -0500

    init & setting post documentation

---
+++ api_project.js
@@ -0,0 +1,23 @@
+define({
+  "name": "Apis",
+  "version": "1.0.1",
+  "description": "Documentacion Apis",
+  "title": "Documentacion Apis",
+  "url": "https://sandbox.pagofacil.tech",
+  "sampleUrl": "https://sandbox.pagofacil.tech",
+  "order": [
+    "PagoEfectivo"
+  ],
+  "template": {
+    "withCompare": false,
+    "withGenerator": false
+  },
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-06-15T22:00:55.920Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
